0e0a61292715ffb9315e00094c86e78722c64a0e
unknown <movinpinsara@gmail.com>
2024-01-16 01:13:44 +0530

    supabase update

--- Supabase/Project/src/App.js
+++ Supabase/Project/src/App.js
@@ -4,11 +4,15 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 const supabase = createClient('https://byxfoopydtgcodwmnaug.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5eGZvb3B5ZHRnY29kd21uYXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ2MzE4MTYsImV4cCI6MjAyMDIwNzgxNn0.Ye68274FT81wc_qQNLLA605VPWa37hChWNIPgHt9gV0')
 
-const App = () => 
-<>
-  <div style={{width: '300px'}}>
-  <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme='dark' queryParams={{ prompt: 'consent'}} />
-  </div>
-</>
+const App = () =>
+  <>
+    <div style={{ width: '35%', padding: "3rem", backgroundColor: "black" }}>
+      <Auth 
+        supabaseClient={supabase} 
+        appearance={{ theme: ThemeSupa }} 
+        theme='dark' 
+        queryParams={{ prompt: 'consent' }} />
+    </div>
+  </>
 
 export default App;
